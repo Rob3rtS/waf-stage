65c0266a0e16e644f2dad6d2098345f971b2cd18
Robert <rob3rt.schuster@gmail.com>
2015-03-21 18:39:58 +0100

    prepared visualization data generation with json

--- waflib/extras/visualization.py
+++ waflib/extras/visualization.py
@@ -17,48 +17,33 @@ import os
 import json
 import random
 import colorsys
-from waflib import Task, Node, Utils, Context
+from waflib import Task, TaskGen, Node, Utils, Context
 
 from waflib.Configure import conf
 
 @conf
 def vis_init(ctx):
 	ctx.vis_registry=VisRegistry(ctx)
-	print('Initialized vr: %r'%ctx.vis_registry)
 
 class VisRegistry:
 	def __init__(self, bld):
 		self.artifacts = {}
-		self.framework={}
+		self.tgens = {}
 		self.bld=bld
-		if (not 'vis_framework' in bld.env) or ('vis_framework' in bld.env and bld.env['vis_framework'] not in ['jit', 'vis']):
-			self.framework['name'] = 'jit'
-		else:
-			self.framework['name'] = bld.env['vis_framework']
-		# framework specific default shape
-		if self.framework['name'] == 'vis':
-			self.framework['default_shape'] = 'box'
-		if self.framework['name'] == 'jit':
-			self.framework['default_shape'] = 'circle'
-		# list of all encountered file extensions for colorization
-		self.file_exts=[]
-		if 'vis_default_color' in bld.env:
-			# disable color generation
-			self.framework['color']=bld.env['vis_default_color']
-			if not re.match('#[A-F0-9]{6}$', self.framework['color']):
-				raise ValueError('Invalid html color: %s'%self.framework['color'])
-		else:
-			self.framework['color']={}
+		# list of all encountered file extensions and feature combinations for colorization
+		self.color_keys=[]
 	def add_artifact(self, _node):
-		self.artifacts[_node.name]=_node
+		self.artifacts[_node.vis_id()]=_node
 		# unique list of file extensions
 		ext=_node.suffix()
-		if ext and not ext in self.file_exts:
-			self.file_exts.append(ext)
-	def get_artifact(self, _name):
-		if not _name in self.artifacts:
-			return None
-		return self.artifacts[_name]
+		if ext and not ext in self.color_keys:
+			self.color_keys.append(ext)
+	def add_tgen(self, _tg):
+		self.tgens[_tg.vis_id()]=_tg
+		# unique list of featuresets
+		fp=_tg.vis_featureset
+		if fp and not fp in self.color_keys:
+			self.color_keys.append(fp)
 	def get_rootelems(self):
 		roots=set()
 		for i in self.artifacts.values():
@@ -68,63 +53,62 @@ class VisRegistry:
 	def create_project(self):
 		product=getattr(Context.g_module, 'APPNAME', 'noname')
 		prod=self.bld.path.find_or_declare(product)
+		prod.vis_tgen='root'
 		if not hasattr(prod, 'vis_children'):
 			prod.vis_children=set()
 		for i in self.get_rootelems():
 			prod.vis_children.add(i)
 			i.vis_parents.add(prod)
-		prod.vis_shape=self.get_shape_map()['root']
 		self.add_artifact(prod)
 		self.project=prod
-	def serialize(self):
+	def serialize_nodes(self):
 		self.create_project()
-		self.vis_nodes=[]
-		self.vis_edges=[]
-		for i in self.artifacts.values():
-			nd={}
-			nd['id']=i.vis_id()
-			nd['label']=i.name
-			self.vis_nodes.append(nd)
-			for j in i.vis_children:
-				ed={}
-				ed['to']=i.vis_id()
-				ed['from']=j.vis_id()
-				self.vis_edges.append(ed)
-		result='''
-var nodes=%s;
-var edges=%s;
-var data = {
-	nodes: nodes,
-	edges: edges
-};
-'''%(json.dumps(self.vis_nodes), json.dumps(self.vis_edges))
-		return result
-
-	def get_shape_map(self):
-		types='program stlib shlib root'.split()
-		vis_shape_map={}
-		if 'vis_shape_flavor' in self.bld.env:
-			flavor=self.bld.env['vis_shape_flavor']
-		else:
-			flavor='mixed'
-		if flavor == 'mixed':
-			# shapes
-			vis_shape_map['program']='circle'
-			vis_shape_map['stlib']='ellipse'
-			vis_shape_map['shlib']='square'
-			vis_shape_map['root']='star'
-		if flavor == 'uniform':
-			for x in types:
-				vis_shape_map[x]=self.framework['default_shape']
-		return (vis_shape_map)
+		# convert the objects to a data structure ready for being dumped as json
+		jr={}
+		for a in self.artifacts:
+			# a (the key) is also the id - create a dict of dicts
+			art=self.artifacts[a]
+			jr[a]={}
+			jr[a]['name']=art.name
+			if art.vis_tgen == 'root':
+				jr[a]['visible']='false'
+			else:
+				jr[a]['visible']='true'
+			# might be redundant, but it should unify handling (see self.serialize_tgens())
+			jr[a]['colorkey']=art.suffix()
+			jr[a]['parents']=[x.vis_id() for x in art.vis_parents]
+			jr[a]['children']=[x.vis_id() for x in art.vis_children]
+			try:
+				jr[a]['tgen']=art.vis_tgen
+			except:
+				# probably happening for the root element
+				print('no tgen for %s'%art.name)
+		return json.dumps(jr)
+	def serialize_tgens(self):
+		# convert the objects to a data structure ready for being dumped as json
+		jr={}
+		for a in self.tgens:
+			# a (the key) is also the id - create a dict of dicts
+			art=self.tgens[a]
+			jr[a]={}
+			jr[a]['name']=art.get_name()
+			jr[a]['visible']='true'
+			jr[a]['colorkey']=art.vis_featureset
+		return json.dumps(jr)
+	def serialize_colors(self):
+		# generate distinct colors
+		colors={}
+		i=0
+		for c in gethtmlcolors(self.color_keys.__len__()):
+			colors[self.color_keys[i]]=c
+			i+=1
+		return json.dumps(colors)
 	def visualize_nodes(self):
-		output='Data.js'
+		# write the data structures to json files
 		def tmp(bld):
-			#~ tgens=bld.get_all_task_gen()
-			#~ for tg in tgens:
-				#~ for tsk in tg.tasks:
-					#~ print('%r\n\n'%tsk)
-			Utils.writef(bld.bldnode.abspath()+os.sep+self.framework['name']+output, bld.vis_registry.serialize())
+			Utils.writef(bld.bldnode.abspath()+os.sep+'wafNodes.json', bld.vis_registry.serialize_nodes())
+			Utils.writef(bld.bldnode.abspath()+os.sep+'wafTaskGens.json', bld.vis_registry.serialize_tgens())
+			Utils.writef(bld.bldnode.abspath()+os.sep+'wafColors.json', bld.vis_registry.serialize_colors())
 		return tmp
 
 
@@ -132,7 +116,8 @@ var data = {
 Task.Task.old_post_run=Task.Task.post_run
 def new_post_run(self):
 	self.old_post_run()
-	#print(self.generator)
+	tg=self.generator
+
 	# explicit deps
 	expl_deps=self.inputs + self.dep_nodes
 	# omitting manual deps for now
@@ -154,107 +139,57 @@ def new_post_run(self):
 	except:
 		# we are in a build context, where vis_registry is missing - likely a configuration check task
 		return
+	vr.add_tgen(tg)
 	for dep in expl_deps + impl_deps:
 		if not hasattr(dep, 'vis_parents'):
 			dep.vis_parents=set()
 		dep.vis_parents.update(set(self.outputs))
+		dep.vis_tgen=tg.vis_id()
 		vr.add_artifact(dep)
-		print(self.generator.get_name())
 	for out in self.outputs:
 		if not hasattr(out, 'vis_children'):
 			out.vis_children=set()
 		out.vis_children.update(expl_deps + impl_deps)
+		out.vis_tgen=tg.vis_id()
 		vr.add_artifact(out)
-	# assign shapes
-	this_class=type(self).__name__
-	for s in vr.get_shape_map().keys():
-		if re.search(s, this_class):
-			for o in self.outputs:
-				o.vis_shape=vr.get_shape_map()[s]
 
 Task.Task.post_run=new_post_run
 
 # extend class Node
 Node.Node.old__init__=Node.Node.__init__
-def vis__init__(self, name, parent):
+def vis_nd__init__(self, name, parent):
 	self.old__init__(name, parent)
 	self.vis_children=set()
 	self.vis_parents=set()
-def vis_get_deps(self):
+def vis_nd_get_deps(self):
 	return self.vis_children
-def vis_get_parents(self):
+def vis_nd_get_parents(self):
 	return self.vis_parents
-def vis_id(self):
+def vis_nd_id(self):
 	m=Utils.md5()
 	m.update(self.abspath().encode())
 	return binascii.hexlify(m.digest()).decode()
-def vis_serialize(self, framework):
-	out=''
-	if not getattr(self, 'vis_shape', ''):
-		self.vis_shape=framework['default_shape']
-	try:
-		if type(framework['color']).__name__ == 'dict':
-			color=framework['color'][self.suffix()]
-		else:
-			color=framework['color']
-	except KeyError:
-		# fallback
-		color='#CCBBBB'
-	if framework['name'] == 'jit':
-		arr=[]
-		out+=\
-""" {
-"id": "%s",
-"name": "%s",
-"data": {
-"$dim": 10,
-"$type": "%s",
-"$color": "%s"
-},
-"""%(self.vis_id(), self.name, self.vis_shape, color)
 
-		if(self.vis_parents or self.vis_children):
-			out+='"adjacencies": [\n'
-			if(self.vis_parents):
-				for d in self.vis_parents:
-					adj=\
-"""{
-"nodeTo": "%s",
-"data": {
-"weight": 1,
-"$type":"arrow",
-"$direction": ["%s", "%s"]
-}
-}
-"""%(d.vis_id(), self.vis_id(), d.vis_id())
-					arr.append(adj)
-			if(self.vis_children):
-				for d in self.vis_children:
-					adj=\
-"""{
-"nodeTo": "%s",
-"data": {
-"weight": 1,
-"$type":"arrow",
-"$direction": ["%s", "%s"]
-}
-}"""%(d.vis_id(), d.vis_id(), self.vis_id())
-					arr.append(adj)
-			out+=',\n'.join(arr)
-			out+=']\n'
-		out+='}\n'
-	if framework['name'] == 'vis':
-		out+='nodes.push({id: "%s", label: "%s", shape: "%s", color: "%s"});\n'%(self.vis_id(), self.name, self.vis_shape, color)
-		if(self.vis_children):
-			for d in self.vis_children:
-				out+='edges.push({from: "%s", to: "%s"});\n'%(d.vis_id(), self.vis_id())
-	return(out)
+Node.Node.__init__=vis_nd__init__
+Node.Node.vis_get_deps=vis_nd_get_deps
+Node.Node.vis_get_parents=vis_nd_get_parents
+Node.Node.vis_id=vis_nd_id
 
-Node.Node.__init__=vis__init__
-Node.Node.vis_serialize=vis_serialize
-Node.Node.vis_get_deps=vis_get_deps
-Node.Node.vis_get_parents=vis_get_parents
-Node.Node.vis_id=vis_id
+# extend class task_gen
+TaskGen.task_gen.old__init__=TaskGen.task_gen.__init__
+def vis_tg__init__(self, *k, **kw):
+	self.old__init__(*k, **kw)
+	# use the same color for all task gens with the same sef of features
+	self.vis_featureset="_".join(sorted(Utils.to_list(self.features)))
+def vis_tg_id(self):
+	# assumption: combination of task generator name and its path is usually unique, so use it as ID
+	tp=self.path.abspath()+os.sep+self.get_name()
+	m=Utils.md5()
+	m.update(tp.encode())
+	return binascii.hexlify(m.digest()).decode()
+
+TaskGen.task_gen.__init__=vis_tg__init__
+TaskGen.task_gen.vis_id=vis_tg_id
 
 #################
 # Color Generator
